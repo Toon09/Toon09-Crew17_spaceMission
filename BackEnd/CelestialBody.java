d92185e6405db9137056d7614ec44bbf26d7d498
Toon09 <103414971+Toon09@users.noreply.github.com>
2023-03-18 14:01:24 +0100

    celestial body update

--- BackEnd/CelestialBody.java
+++ BackEnd/CelestialBody.java
@@ -2,6 +2,8 @@ package BackEnd;
 
 //to create model of solar system
 public class CelestialBody {
+
+    private String name = "";
     private double mass;
 
     private double[] pos;
@@ -18,15 +20,26 @@ public class CelestialBody {
 
     }
 
+    CelestialBody(String name, double mass, double[] innitPos, double[] innitVel, double[] innitAcc){
+        this.name = name;
+        setMass(mass);
+        setPos(innitPos);
+        setVel(innitVel);
+        setAcc(innitAcc);
+
+    }
+
 
     //getters
     public double[] getPos(){ return pos; }
     public double[] getVel(){ return vel; }
     public double[] getAcc(){ return acc; }
     public double getMass(){ return mass; }
+    public String getName(){ return name; }
 
     //setters
     public void setMass(double mass){ this.mass = mass; }
+    public void setName(String name){ this.name = name; }
     public void setPos(double[] pos){
         this.pos[0] = pos[0];
         this.pos[1] = pos[1];
@@ -41,6 +54,31 @@ public class CelestialBody {
         this.acc[0] = acc[0];
         this.acc[1] = acc[1];
         this.acc[2] = acc[2];
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if( !(obj instanceof CelestialBody) )
+            return false;
+
+        CelestialBody temp = (CelestialBody)obj;
+
+        for(int i=0; i<3; i++){
+            if( pos[i] != temp.getPos()[i] )
+                return false;
+        }
+
+        for(int i=0; i<3; i++){
+            if( vel[i] != temp.getVel()[i] )
+                return false;
+        }
+
+        for(int i=0; i<3; i++){
+            if( acc[i] != temp.getAcc()[i] )
+                return false;
+        }
+
+        return true;
     }
 
 }
--- BackEnd/Models/Gravity0.java
+++ BackEnd/Models/Gravity0.java
@@ -5,14 +5,25 @@ import BackEnd.CelestialBody;
 public class Gravity0 implements Model {
     /*
      * 3D model
+     * model is:
+     * 
+     * k'' = G*(k' - k)/r^3
+     * 
+     * k is 3d
+     * r = distance from object i to another object j; i != j
+     * G =  6.6743*10^(-20)
      */
 
+    private static final double G = 6.6743 * Math.pow(10, -20);
     private CelestialBody[] bodies;
 
     public Gravity0(){
         bodies = new CelestialBody[0];
     }
 
+    public CelestialBody getBody(int index){ return bodies[index]; }
+
+    //only for 1 body
     public void addBody(CelestialBody body){
         CelestialBody[] temp = new CelestialBody[ bodies.length + 1 ];
         for(int i=0; i<bodies.length; i++){
@@ -23,6 +34,7 @@ public class Gravity0 implements Model {
         bodies = temp;
     }
 
+    //for a set of bodies
     public void addBody(CelestialBody[] Nbody){
         CelestialBody[] temp = new CelestialBody[ bodies.length + Nbody.length ];
 
@@ -37,46 +49,66 @@ public class Gravity0 implements Model {
         bodies = temp;
     }
 
-    @Override
-    public void setX(double x) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setX'");
+    public double getDistance(CelestialBody body1, CelestialBody body2){
+        double a = ( body1.getPos()[0] - body2.getPos()[0] );
+        double b = ( body1.getPos()[1] - body2.getPos()[1] );
+        double c = ( body1.getPos()[2] - body2.getPos()[2] );
+
+        return Math.sqrt( a*a + b*b + c*c );
     }
 
-    @Override
-    public void setY(double y) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setY'");
-    }
+    //settings
+    public void setPos(int index, double[] pos){ bodies[index].setPos(pos); }
+    public void setVel(int index, double[] vel){ bodies[index].setPos(vel); }
+    public void setAcc(int index, double[] acc){ bodies[index].setAcc(acc); }
 
-    @Override
-    public void setYD(double yD) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setYD'");
-    }
 
-    @Override
-    public double getX() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getX'");
-    }
 
-    @Override
-    public double getY() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getY'");
-    }
+
+    
 
     @Override
     public double _1Deriv() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method '_1Deriv'");
+
+        //update all celestial bodies one by one with one another
+
+        return 0; //doesnt return a val in specific bc its 3d, instead is saved in the bodies themselves
     }
 
     @Override
     public double _2Deriv() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method '_2Deriv'");
+
+
+        return 0; //doesnt return a val in specific bc its 3d, instead is saved in the bodies themselves
     }
 
+
+
+    
+    // ########################### DO NOT IMPLEMENT
+
+    @Override //////////////do not implement
+    public void setX(double x) {
+        throw new UnsupportedOperationException("Unimplemented method 'setX'");
+    }
+ 
+    @Override //////////////do not implement
+    public void setY(double y) {
+        throw new UnsupportedOperationException("Unimplemented method 'setY'");
+    }
+
+    @Override //////////////do not implement
+    public double getX() {
+        throw new UnsupportedOperationException("Unimplemented method 'getX'");
+    }
+
+    @Override //////////////do not implement
+    public double getY() {
+        throw new UnsupportedOperationException("Unimplemented method 'getY'");
+    }
+
+    @Override //////////////do not implement
+    public void setYD(double yD) {
+        throw new UnsupportedOperationException("Unimplemented method 'setYD'");
+    }
 }
