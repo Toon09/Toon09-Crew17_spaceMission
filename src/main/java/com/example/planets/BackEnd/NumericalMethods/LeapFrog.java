0749cc311a67751e161eed6b913525a485a6809c
Toon09 <103414971+Toon09@users.noreply.github.com>
2023-05-04 20:37:13 +0200

    adding leapFrog file

---
+++ src/main/java/com/example/planets/BackEnd/NumericalMethods/LeapFrog.java
@@ -0,0 +1,16 @@
+package com.example.planets.BackEnd.NumericalMethods;
+
+import com.example.planets.BackEnd.Models.Model3D;
+
+// https://en.wikipedia.org/wiki/Leapfrog_integration
+public class LeapFrog implements NumSolver{
+    @Override
+    public void step(Model3D model, double dt) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
