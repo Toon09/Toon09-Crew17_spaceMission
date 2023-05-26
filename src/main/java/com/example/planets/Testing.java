2c713802d6db13818a74902620cda222b71cbac2
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-05-26 11:00:39 +0200

    added stuff

---
+++ src/main/java/com/example/planets/Testing.java
@@ -0,0 +1,28 @@
+package com.example.planets;
+
+import com.example.planets.BackEnd.Models.Gravity0;
+import com.example.planets.BackEnd.NumericalMethods.Euler;
+
+import java.util.Arrays;
+
+public class Testing {
+    static Gravity0 model = new Gravity0(0, Math.PI / 2.0, new Euler());
+    public static void main(String[] args) {
+        //creating the initial population
+        Gravity0[] population = new Gravity0[20];
+        while (true){
+            for (int i=0; i<20; i++){
+
+            }
+        }
+
+
+    }
+    public double fit(double[] ship, double[] titan){
+        double distance = 0;
+        for (int i =0; i<3; i++){
+            distance+= titan[i]-ship[i];
+        }
+        return distance;
+    }
+}
