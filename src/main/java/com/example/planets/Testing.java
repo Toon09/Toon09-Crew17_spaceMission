aba1a636c99b5798501bd367d63b38137c18f29b
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-05-26 11:22:41 +0200

    testing

--- src/main/java/com/example/planets/Testing.java
+++ src/main/java/com/example/planets/Testing.java
@@ -4,27 +4,48 @@ import com.example.planets.BackEnd.Models.Gravity0;
 import com.example.planets.BackEnd.NumericalMethods.Euler;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Testing {
     static Gravity0 model = new Gravity0(0, Math.PI / 2.0, new Euler());
+    static double phaseTime =2;
     public static void main(String[] args) {
-        //creating the initial population
+        //initialize the initial population
         Gravity0[] population = new Gravity0[20];
+        //create the initial plan for the ships
         double[][] initialPlan = new double[20][5];
-        initialPlan[0] ={}
-        while (true){
-            for (int i=0; i<20; i++){
-                population[i] = new Gravity0(0, Math.PI / 2.0, new Euler());
-            }
+        initialPlan[0] = new double[]{0, 2, 11, 11, 11};
+        Random random = new Random();
+        for (int i = 1; i < 20; i++) {
+            initialPlan[i] = new double[]{initialPlan[i - 1][0] + initialPlan[i - 1][1],initialPlan[i - 1][0] + initialPlan[i - 1][1]+phaseTime, random.nextDouble(3), random.nextDouble(3), random.nextDouble(3)};
+        }
+        //create a new population of Gravity 0
+        for (int i = 0; i < 20; i++) {
+            population[i] = new Gravity0(0, Math.PI / 2.0, new Euler(), changeABit(initialPlan));
+        }
+        while (true) {
+
+
         }
 
 
     }
-    public double fit(double[] ship, double[] titan){
+
+    public double fit(double[] ship, double[] titan) {
         double distance = 0;
-        for (int i =0; i<3; i++){
-            distance+= titan[i]-ship[i];
+        for (int i = 0; i < 3; i++) {
+            distance += titan[i] - ship[i];
         }
         return distance;
+    }
+
+    public static double[][] changeABit(double[][] oryginal) {
+        double[][] changed = new double[oryginal.length][oryginal[0].length];
+        changed[0] = oryginal[0];
+        Random random = new Random();
+        for (int i = 1; i < oryginal.length; i++){
+            changed[i]= new double[]{oryginal[i][0],oryginal[i][1],oryginal[i][2]+random.nextDouble(2),oryginal[i][3]+random.nextDouble(2),oryginal[i][4]+random.nextDouble(3)};
+        }
+        return changed;
     }
 }
