c103a414d5098091d714c24fefd85cbde27ddce3
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-06-13 16:08:11 +0200

    Controler interface, Landing module

--- src/main/java/com/example/planets/BackEnd/CelestialEntities/LandingModule.java
+++ src/main/java/com/example/planets/BackEnd/CelestialEntities/LandingModule.java
@@ -2,7 +2,8 @@ package com.example.planets.BackEnd.CelestialEntities;
 
 public class LandingModule extends Spaceship{
     private Controler controler;
-    public LandingModule(double mass, double[] pos, double[] vel) {
+    public LandingModule(double mass, double[] pos, double[] vel, Controler controler) {
         super(mass, pos, vel);
+        this.controler = controler;
     }
 }
