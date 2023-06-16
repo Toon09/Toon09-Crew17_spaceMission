4a4319ff1e5c741c18142ed09f232f3c1b36f8c7
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-06-16 10:59:31 +0200

    gravity for landing

--- src/main/java/com/example/planets/BackEnd/Trajectory/OpenLoop.java
+++ src/main/java/com/example/planets/BackEnd/Trajectory/OpenLoop.java
@@ -21,43 +21,40 @@ public class OpenLoop implements IControler {
     private double g;
     @Override public Planning claclateThrust(double initialX, double initialY, double initialTheta,
                                              double acceleration, double torque, double gravity) {
-
-                x = initialX;
-                y = initialY;
-                theta = initialTheta;
-                u = acceleration;
-                v = torque;
-                g = gravity;
+        x = initialX;
+        y = initialY;
+        theta = initialTheta;
+        u = acceleration;
+        v = torque;
+        g = gravity;
         return null;
     }
 
-            // Method to update the spaceship's position and orientation over time
-            public void update(double dt) {
-                // Calculate the second derivatives (accelerations)
-                double xAcceleration = u * Math.sin(theta);
-                double yAcceleration = u * Math.cos(theta) - g;
-                double thetaAcceleration = v;
+    // Method to update the spaceship's position and orientation over time
+    public void update(double dt) {
+        // Calculate the second derivatives (accelerations)
+        double xAcceleration = u * Math.sin(theta);
+        double yAcceleration = u * Math.cos(theta) - g;
+        double thetaAcceleration = v;
 
-                // Update the position and orientation using Euler's method
-                x += xAcceleration * dt * dt / 2;
-                y += yAcceleration * dt * dt / 2;
-                theta += thetaAcceleration * dt * dt / 2;
-            }
+        // Update the position and orientation using Euler's method
+        x += xAcceleration * dt * dt / 2;
+        y += yAcceleration * dt * dt / 2;
+        theta += thetaAcceleration * dt * dt / 2;
+    }
 
-            // Getters for position and orientation
-            public double getX() {
-                return x;
-            }
+    // Getters for position and orientation
+    public double getX() {
+        return x;
+    }
 
-            public double getY() {
-                return y;
-            }
+    public double getY() {
+        return y;
+    }
 
-            public double getTheta() {
-                return theta;
-            }
-
-
+    public double getTheta() {
+        return theta;
+    }
 
     @Override
     public Planning creatPlan() {
