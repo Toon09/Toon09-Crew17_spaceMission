f615d4e81ccca35b0bf8547fd420faf00fe0b389
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-06-16 12:14:04 +0200

    gravity for landing

--- src/main/java/com/example/planets/BackEnd/Trajectory/OpenLoop.java
+++ src/main/java/com/example/planets/BackEnd/Trajectory/OpenLoop.java
@@ -22,6 +22,9 @@ public class OpenLoop implements IControler {
     private double v; // Total
     private Gravity0 gravity;
     public OpenLoop(Spaceship ship, CelestialBody titan, LandingModel module){
+        titan.setPos(new double[]{0, -2574, 0});
+        ship.setPos(new double[]{0, 300000, 0});
+        module.setPos(new double[]{0, 300000, 0});
         this.gravity = new Gravity0(ship, titan, new LandingModel[]{module});
     }
 
