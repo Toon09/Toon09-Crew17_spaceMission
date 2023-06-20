02be9ecc81fc1cde23fdaa34f0c135c41a5521a0
MereutaVasile <mereuta.vasile01@gmail.com>
2023-06-20 14:41:02 +0200

    Numerical error fixed

--- src/main/java/com/example/planets/BackEnd/Models/GravityForLanding.java
+++ src/main/java/com/example/planets/BackEnd/Models/GravityForLanding.java
@@ -2,45 +2,52 @@ package com.example.planets.BackEnd.Models;
 
 import com.example.planets.BackEnd.CelestialEntities.CelestialBody;
 import com.example.planets.BackEnd.CelestialEntities.Spaceship;
+import com.example.planets.BackEnd.Trajectory.LandingModel;
 
 public class GravityForLanding {
-//    private CelestialBody titan;
-//    private static Spaceship ship;
-//    private LandingModule module;
-//    private double g = 1.352; // m/s^2
-//    private Gravity0 gravity0;
-//
-//
-//    public GravityForLanding(CelestialBody titan, Spaceship ship, LandingModule module) {
-//        this.titan = titan;
-//        this.ship = ship;
-//        this.module = module;
-//        this.gravity0 = new Gravity0(ship, titan, module);
-//        titan.setPos(new double[]{0, -2574, 0});
-//        ship.setPos(new double[]{0, 300000, 0});
-//        module.setPos(new double[]{0, 300000, 0});
+    private CelestialBody titan;
+    private static Spaceship ship;
+    private LandingModel module;
+    private double g = 1.352; // m/s^2
+    private Gravity0 gravity0;
+
+
+    public GravityForLanding(CelestialBody titan, Spaceship ship, LandingModel module) {
+        this.titan = titan;
+        this.ship = ship;
+        this.module = module;
+        this.gravity0 = new Gravity0(titan, module);
+        titan.setPos(new double[]{0, -2574, 0});
+        ship.setPos(new double[]{0, 300000, 0});
+        module.setPos(new double[]{0, 300000, 0});
+    }
+
+    private void applyGravity() {
+        module.addAcc(new double[]{0, g,0});
+    }
+
+    //force of gravity = m*g
+    //g = 1.352 m/s^2
+    //m = 42000 kg
+    //F = 5678,4 N
+    private double calculateForces() {
+        double m = 4200;
+        double g = 1.352;
+        double F = g * m;
+        double d = 300;
+        double t = Math.sqrt((2 * d) / g);
+        System.out.println("m = " + m);
+        System.out.println("g = " + g);
+        System.out.println("F = " + F);
+        System.out.println("d = " + d);
+        System.out.println("t = " + t);
+        System.out.println("F*t = " + F * t);
+        return F*t;
+    }
+
+//    public void updateGravity(double dt) {
+//        g += g*dt;
 //    }
-//
-//    private void applyGravity() {
-//        module.addAcc(new double[]{0, g,0});
-//    }
-//
-//    //force of gravity = m*g
-//    //g = 1.352 m/s^2
-//    //m = 42000 kg
-//    //F = 5678,4 N
-//    private double calculateForces() {
-//        double m = 4200;
-//        double g = 1.352;
-//        double F = g * m;
-//        double d = 300;
-//        double t = Math.sqrt((2 * d) / g);
-//        System.out.println("m = " + m);
-//        System.out.println("g = " + g);
-//        System.out.println("F = " + F);
-//        System.out.println("d = " + d);
-//        System.out.println("t = " + t);
-//        System.out.println("F*t = " + F * t);
-//        return F*t;
-//    }
+
+
 }
