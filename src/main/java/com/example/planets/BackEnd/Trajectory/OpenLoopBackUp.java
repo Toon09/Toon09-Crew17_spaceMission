516c0cd48d11663799413eb329c93f6f69216ee4
Adel Asaad <AdelAsaad1997@gmail.com>
2023-06-26 14:53:27 +0200

    fix not go into loop

--- src/main/java/com/example/planets/BackEnd/Trajectory/OpenLoopBackUp.java
+++ src/main/java/com/example/planets/BackEnd/Trajectory/OpenLoopBackUp.java
@@ -18,10 +18,11 @@ public  class OpenLoopBackUp {
     static double THRUST;
     double vx;
     double vy;
-    double thrust;
-    double torque;
+    double thrust, torque;
+  static  double  SIMULATION_TIME;
+
     double EPSILON_X;
-    static double SIMULATION_TIME;
+
     double TIME_STEP;
     double DELTA_X;
     static double MAX_ANGLE;
@@ -35,7 +36,7 @@ public  class OpenLoopBackUp {
         this.thrust = thrust;
         this.torque = torque;
         this.EPSILON_X = EPSILON_X;
-        this.SIMULATION_TIME = SIMULATION_TIME;
+        this.SIMULATION_TIME = 10000000;
         this.TIME_STEP = TIME_STEP;
         this.DELTA_X = DELTA_X;
 
@@ -46,9 +47,10 @@ public  class OpenLoopBackUp {
 
         // Calculate the thrust force
         thrust[0] = Math.sqrt(vx * vx + vy * vy) < MAX_SPEED ? THRUST : 0;
-
+        System.out.println("thrust[0]"+thrust[0]);
         // Calculate the torque
         thrust[1] = Math.abs(theta % (2 * Math.PI)) < MAX_ANGLE ? TORQUE : 0;
+        System.out.println("thrust[1]"+thrust[1]);
         return thrust;
     }
 
@@ -61,7 +63,6 @@ public  class OpenLoopBackUp {
         double vy = 0; // Vertical velocity (m/s)
         double omega = 0; // Angular velocity (rad/s)
         double GRAVITY= 1.352 * Math.pow(10, -3);
-
         // Landing conditions
         double DELTA_X = 0.1; // m
         double EPSILON_X = 0.1; // m/s
@@ -72,16 +73,15 @@ public  class OpenLoopBackUp {
 double time = 0; // Current simulation time (seconds)
 
         // Simulate the landing process using numerical integration
-        while (time < SIMULATION_TIME)
+        while ( true ){
 
-
-
-    {
         double[] thrust = calculateThrust(x, y, vx, vy, theta, omega);
 
         // Update the state using Euler's method
         x += vx * TIME_STEP;
+        System.out.println("x"+x);
         y += vy * TIME_STEP;
+        System.out.println("y"+y);
         vx += thrust[0] * Math.sin(theta) * TIME_STEP;
         vy += thrust[0] * Math.cos(theta) * TIME_STEP - GRAVITY * TIME_STEP;
         theta += omega * TIME_STEP;
@@ -90,16 +90,12 @@ double time = 0; // Current simulation time (seconds)
         if (Math.abs(x) <= DELTA_X && Math.abs(y) <= DELTA_X && Math.abs(vx) <= EPSILON_X
         && Math.abs(vy) <= EPSILON_Y && Math.abs(theta % (2 * Math.PI)) <= DELTA_THETA) {
         System.out.println("Safe landing achieved!");
+
         break;
         }
 
         time += TIME_STEP;
         }
-
-
-
-
-
     };
         }
 
